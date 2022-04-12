20f15c589cb18394010aeba8f0a1730dbd7e2e80
incelsub <ataiasrblx+uwu@gmail.com>
2022-04-12 19:59:14 +1200

    Created loader script
    Loads script based on game

---
+++ loader.lua
@@ -0,0 +1,23 @@
+--> Base URL of Juice Hub
+local baseUrl = "https://github.com/incelsub/Juice-Hub/main"
+
+--> Load init script
+loadstring(game:HttpGet(baseUrl .. "/init.lua"))()
+
+--> Handler
+local games = {
+  [placeIdHere] = {
+    name = "Game Name Here",
+    url = "/GameNameHere/url-to-script.lua"
+  }
+}
+
+local game = games[game.PlaceId]
+
+--> Invalid game check
+if (not game) then
+  return client:Kick("This is not a game supported by Juice Hub.")
+end
+
+--> Load game script
+loadstring(game:HttpGet(baseUrl .. game.url))()
