a396411a2d426af434f6a07ca5ced2de313b2278
One Shot <75005252+OneShotKillHD@users.noreply.github.com>
2022-04-13 21:47:55 -0700

    LOL

--- src/Loader/Loader.lua
+++ src/Loader/Loader.lua
@@ -1,4 +1,18 @@
+-- coded like a ape to make people question why they live.
 return function(game)
+  local games = require("./games");
+  local gameId = game.PlaceId;
   
+  local gameInfo;
   
+  for i,v in next, games do
+      for a,b in next, v do
+        if (a == tostring("GameId")) then 
+          if (tonumber(b) == gameId) then
+              gameInfo = b;
+              break;
+            end;
+         end;
+      end;
+    end;
  end;
