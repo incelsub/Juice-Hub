decd21ecd635f76ae0e8b06e8ca9eec5778b4768
One Shot <75005252+OneShotKillHD@users.noreply.github.com>
2022-04-13 21:55:36 -0700

    Update Loader.lua

--- src/Loader/Loader.lua
+++ src/Loader/Loader.lua
@@ -3,16 +3,6 @@ return function(game)
   local games = require("./games");
   local gameId = game.PlaceId;
   
-  local gameInfo;
   
-  for i,v in next, games do
-      for a,b in next, v do
-        if (a == tostring("GameId")) then 
-          if (tonumber(b) == gameId) then
-              gameInfo = b;
-              break;
-            end;
-         end;
-      end;
-    end;
+
  end;
