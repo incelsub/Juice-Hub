419134a25dabca16317113a92efde1fb82fb5ee3
One Shot <75005252+OneShotKillHD@users.noreply.github.com>
2022-04-13 21:52:14 -0700

    Update games.lua

--- src/Loader/games.lua
+++ src/Loader/games.lua
@@ -1,8 +1,8 @@
 return {
-  ["Anomic"] = { -- File Name
-    ["GameId"] = 4581966615, -- Game Name
+  [4581966615] = { -- Game ID
+    ["GameName"] = tostring("Anomic"), -- Game File
     ["Finished"] = false -- Should the game be allowed the be executed
     ["HaveOralWithFrosty"] = false -- hes kinda gay but doesnt like cock in his mouth?
-    ["LolingWinning"] = true -- He loves to get blow jobs while working
+    ["LolingWinning"] = false -- He loves to get blow jobs while working
   }
 };
