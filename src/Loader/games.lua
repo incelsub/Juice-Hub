61b08cd8c4e7bc24d3163a544c0408a5aced43e0
One Shot <75005252+OneShotKillHD@users.noreply.github.com>
2022-04-13 21:35:32 -0700

    Added layout for games

--- src/Loader/games.lua
+++ src/Loader/games.lua
@@ -1,3 +1,7 @@
 return {
--- fortnite
+  ["Anomic"] = { -- File Name
+    ["GameId"] = 4581966615, -- Game Name
+    ["Finished"] = false -- Should the game be allowed the be executed
+    ["HaveOralWithFrosty"] = false -- hes kinda gay but doesnt like cock in his mouth?
+  }
 };
