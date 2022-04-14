3163bb62a77c008ee1c9457aaa0e9bf854e15e56
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2022-04-14 00:50:18 -0400

    Update games.lua

--- src/Loader/games.lua
+++ src/Loader/games.lua
@@ -3,5 +3,6 @@ return {
     ["GameId"] = 4581966615, -- Game Name
     ["Finished"] = false -- Should the game be allowed the be executed
     ["HaveOralWithFrosty"] = false -- hes kinda gay but doesnt like cock in his mouth?
+    ["LolingWinning"] = true -- He loves to get blow jobs while working
   }
 };
