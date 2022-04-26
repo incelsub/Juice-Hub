e2e09cd2a7c1204af6424ae34201b18bfb84cac4
incelsub <94930119+incelsub@users.noreply.github.com>
2022-04-27 00:45:47 +1200

    fixed

--- loader.lua
+++ loader.lua
@@ -3,12 +3,12 @@ local games = {
   [13822889] = "/LT2/main.lua"
 }
 
-local game = games[game.PlaceId]
+local gameUrl = games[game.PlaceId]
 
 --> Invalid game check
-if (not game) then
+if (not gameUrl) then
   return game:GetService("Players").LocalPlayer:Kick("This is not a game supported by Juice Hub.")
 end
 
 --> Load script
-loadstring(game:HttpGet("https://raw.githubusercontent.com/incelsub/Juice-Hub/main" .. game))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/incelsub/Juice-Hub/main" .. gameUrl))()
