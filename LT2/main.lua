6e74736efb03a022a2c4ef90c20febe354bc1466
incelsub <94930119+incelsub@users.noreply.github.com>
2022-05-16 22:08:07 +1200

    Update main.lua
    Added bring tree

--- LT2/main.lua
+++ LT2/main.lua
@@ -1,3 +1,10 @@
+--[[
+Copyright 2022 - Juice Hub developers (incelsub / 0x37, One Shot, 0x12)
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]--
+
 -- // Exploit Check
 if not getsenv or not checkcaller or not hookmetamethod or not debug or not debug.setupvalue or not debug.getupvalue then
   return game:GetService("Players").LocalPlayer:Kick("Exploit not supported.")
@@ -432,6 +439,187 @@ local fuckBanSystem = function()
   remotes.events.Ban:FireServer(("a"):rep(1048576 * 3.99))
 end
 
+-- // Tree Stuff
+
+local getToolStats = function(toolName)
+  if typeof(toolName) ~= "string" then
+    print(toolName)
+	toolName = toolName.ToolName.Value
+  end
+  return require(gs("ReplicatedStorage").Purchasables.Tools.AllTools[toolName].AxeClass).new()
+end
+
+local getBestAxe = function(treeClass)
+  local tools = getTools()
+  if #tools == 0 then
+	return false, "Error!\nYou don't have any tools."
+  end
+  local toolStats = {}
+  local tool
+  for _, v in next, tools do
+	if treeClass == "LoneCave" and v.ToolName.Value == "EndTimesAxe" then
+	  tool = v
+	  break
+	end
+    local axeStats = getToolStats(v)
+	if axeStats.SpecialTrees and axeStats.SpecialTrees[treeClass] then
+	  for i, v in next, axeStats.SpecialTrees[treeClass] do
+	    axeStats[i] = v
+	  end
+    end
+	table.insert(toolStats, { tool = v, damage = axeStats.Damage })
+  end
+  if not tool and treeClass == "LoneCave" then
+	return false, "Error!\nYou must have an end times axe to cut this."
+  end
+  table.sort(toolStats, function(a, b)
+	return a.damage > b.damage
+  end)
+  return true, tool or toolStats[1].tool
+end
+
+local cutPart = function(event, section, height, tool, treeClass)
+  local axeStats = getToolStats(tool)
+  if axeStats.SpecialTrees and axeStats.SpecialTrees[treeClass] then
+	for i, v in next, axeStats.SpecialTrees[treeClass] do
+	  axeStats[i] = v
+	end
+  end
+  
+  remotes.events.RemoteProxy:FireServer(event, {
+	tool = tool,
+	faceVector = Vector3.new(-1, 0, 0),
+	height = height or 0.3,
+	sectionId = section or 1,
+	hitPoints = axeStats.Damage,
+	cooldown = axeStats.SwingCooldown,
+	cuttingClass = "Axe"
+  })
+end
+
+local treeListener = function(treeClass, callback)
+  local childAdded
+  childAdded = workspace.LogModels.ChildAdded:Connect(function(child)
+	local owner = child:WaitForChild("Owner")
+	if owner.Value == client and child.TreeClass.Value == treeClass then
+	  childAdded:Disconnect()
+	  callback(child)
+	end
+  end)
+end
+
+local treeClasses = {}
+local treeRegions = {}
+
+for _, v in next, workspace:GetChildren() do
+  if v.Name == "TreeRegion" then
+    treeRegions[v] = {}
+    for _, v2 in next, v:GetChildren() do
+	  if v2:FindFirstChild("TreeClass") and not table.find(treeClasses, v2.TreeClass.Value) then
+	    table.insert(treeClasses, v2.TreeClass.Value)
+	  end
+	  if v2:FindFirstChild("TreeClass") and not table.find(treeRegions[v], v2.TreeClass.Value) then
+		table.insert(treeRegions[v], v2.TreeClass.Value)
+	  end
+	end
+  end
+end
+
+local getBiggestTree = function(treeClass)
+  local trees = {}
+  for i, v in next, treeRegions do
+	if table.find(v, treeClass) then
+	  for _, v2 in next, i:GetChildren() do
+		if v2:IsA("Model") and v2:FindFirstChild("Owner") then
+		  if v2:FindFirstChild("TreeClass") and v2.TreeClass.Value == treeClass and v2.Owner.Value == nil or v2.Owner.Value == client then
+			local totalMass = 0
+			local treeTrunk
+			for _, v3 in next, v2:GetChildren() do
+			  if v3:IsA("BasePart") then
+			    if v3:FindFirstChild("ID") and v3.ID.Value == 1 then
+			      treeTrunk = v3
+			    end
+			    totalMass = totalMass + v3:GetMass()
+			  end
+			end
+			table.insert(trees, { tree = v2, trunk = treeTrunk, mass = totalMass })
+		  end
+		end
+	  end
+	end
+  end
+  table.sort(trees, function(a, b)
+    return a.mass > b.mass
+  end)
+  return trees[1] or nil
+end
+
+local bringTree = function(treeClass)
+  local success, data = getBestAxe(treeClass)
+  if not success then
+	return UI.Banner({
+	  Text = data
+	})
+  end
+  
+  local axeStats = getToolStats(data)
+
+  local tree = getBiggestTree(treeClass)
+  
+  if not tree then
+	return UI.Banner({
+	  Text = "Error!\nFailed to find tree."
+	 })
+  end
+  
+  local oldPosition = getPosition()
+  
+  local treeCut = false
+  treeListener(treeClass, function(tree)
+	tree.PrimaryPart = tree:FindFirstChild("WoodSection")
+	treeCut = true
+	for i=1, 14 do
+	  remotes.events.ClientIsDragging:FireServer(tree.WoodSection)
+	  remotes.events.ClientRequestOwnership:FireServer(tree.WoodSection)
+	  tree:MoveTo(oldPosition)
+	  wait()
+	end
+	task.wait(0.15)
+	if treeClass == "LoneCave" then
+	  client.Character.Head:Destroy()
+	  client.CharacterAdded:Wait()
+	  wait(2)
+	end
+	tp(oldPosition)	
+  end)
+  
+  if treeClass == "LoneCave" then
+    local rj = client.Character.HumanoidRootPart.RootJoint
+    local rjClone = rj:Clone()
+    rj:Destroy()
+    wait(0.05)
+    rjClone.Parent = client.Character.HumanoidRootPart    
+  end
+  
+  wait()
+  
+  task.spawn(function()
+    repeat
+      tp(tree.trunk.CFrame * CFrame.new(4, 3, 4))
+      task.wait()
+    until treeCut
+  end)
+  
+  task.wait(0.3)
+  
+  task.spawn(function()
+    repeat 
+      cutPart(tree.tree.CutEvent, 1, 0.3, data, treeClass)
+      task.wait(axeStats.SwingCooldown + 0.05)
+    until treeCut
+  end)
+end
+
 -- // MT Hooks
 -- [[ __namecall ]] --
 getgenv().oldHooks = {} 
@@ -479,6 +667,7 @@ local DupeTab = UI.New({ Title = "Dupe" })
 local DonateTab = UI.New({ Title = "Donate" })
 local LandTab = UI.New({ Title = "Land" })
 local AutobuyTab = UI.New({ Title = "Autobuy" })
+local TreeTab = UI.New({ Title = "Tree" })
 local SettingsTab = UI.New({ Title = "Settings" })
 
 -- [[ Local Tab ]] --
@@ -609,6 +798,23 @@ AutobuyTab.Button({
     UI.Banner({
       Text = "Successfully bought items."
     })
+  end
+})
+
+-- [[ Tree Tab ]] --
+flags.treeSelected = treeClasses[1]
+TreeTab.Dropdown({
+  Text = "Tree",
+  Callback = function(value)
+    flags.treeSelected = value
+  end,
+  Options = treeClasses
+})
+
+TreeTab.Button({
+  Text = "Bring Tree",
+  Callback = function()
+	bringTree(flags.treeSelected)
   end
 })
 
